ce3b38100e0f8e182e4665450cada8d9d11fb68b
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-06-19 15:03:40 +0700

    Add HelpetApp

---
+++ Helpet/HelpetApp.swift
@@ -0,0 +1,19 @@
+//
+//  HelpetApp.swift
+//  Helpet
+//
+//  Created by Efim Nikitin on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelpetApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
